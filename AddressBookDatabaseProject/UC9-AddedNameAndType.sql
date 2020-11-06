cf883328bcdd02bbd6b2cbcbc85522f73380db67
BlaKe00786 <sameershaikh403@gmail.com>
2020-11-06 11:45:39 +0530

    [Sameer]Add: added name and type column.

---
+++ AddressBookDatabaseProject/UC9-AddedNameAndType.sql
@@ -0,0 +1,6 @@
+alter table AddressBook add type varchar(50),name varchar(50)
+update AddressBook set type='Agent' where First_Name='Sven'
+update AddressBook set name='ProfessionalContacts' where First_Name='Sven'
+update AddressBook set type='Secretary' where First_Name='Teressa'
+update AddressBook set name='ImportantContacts' where First_Name='Teressa'
+select * from AddressBook
