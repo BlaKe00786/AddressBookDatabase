07df0ed257e6c2164efa269dcbd0b2bdf7835537
BlaKe00786 <sameershaikh403@gmail.com>
2020-11-06 11:56:17 +0530

    [Sameer]Add: ability to Add friends and Family.

---
+++ AddressBookDatabaseProject/UC11-AddFriendsAndFamily.sql
@@ -0,0 +1,11 @@
+insert into AddressBook values
+('Poorvin','Desai','sandip','Valsad','Gujurat',729,767853458,'poorvin@gmail.com','Family','poorvinJob'),
+('Poorvin','Desai','sandip','Valsad','Gujurat',729,767853458,'poorvin@gmail.com','Friend','poorvinJob'),
+
+('Abhijeet','Gaikwad','ashokanagar','nasik','Maharashtra',7237,76683438,'abhijeet@gmail.com','Family','Abhifriend'),
+('Abhijeet','Gaikwad','ashokanagar','nasik','Maharashtra',7237,76683438,'abhijeet@gmail.com','Friend','Abhifriend'),
+
+('Romesh','Sonar','navsari123','navsari','gujarat',123433,777234377,'Romesh@gmail.com','Friend','Dost'),
+
+('Arbaz','Ali','Delhi123','London','Haryana',789,77788,'Arbaz@gmail.com','Family','Bro')
+select * from AddressBook
